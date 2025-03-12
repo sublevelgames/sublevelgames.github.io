52085d77832d33447d506b061e39ed71cbd803d7
sublevelgames <sublevelgames@gmail.com>
2025-03-12 09:30:35 +0900

    fix tag

--- rakefile.rb
+++ rakefile.rb
@@ -22,16 +22,19 @@ task :generate_tags do
 
   # Process each unique tag
   all_tags.uniq.each do |tag|
-    # Create tag page filename (slugify the tag)
-    tag_slug = tag.gsub(/[^\w]/, '-').downcase.gsub(/-+/, '-').gsub(/^-|-$/, '')
-    tag_file = "_tags/#{tag_slug}.md"
+    # 첫 글자(이모지)를 제외한 태그 텍스트 추출
+    tag_without_emoji = tag[1..-1]
+    # 태그 슬러그 생성
+    tag_slug = tag_without_emoji.strip.gsub(/\s+/, '-').gsub(/[^\w\-]/, '').downcase
     
     # Create tag page if it doesn't exist
+    tag_file = "_tags/#{tag_slug}.md"
+    
     unless File.exist?(tag_file)
       File.open(tag_file, 'w') do |file|
         file.write("---\nlayout: tag\ntag: \"#{tag}\"\npermalink: /tags/#{tag_slug}/\n---\n")
       end
-      puts "Created tag page for: #{tag}"
+      puts "Created tag page for: #{tag} (slug: #{tag_slug})"
     end
   end
   
--- tag-generator-script.rb
+++ tag-generator-script.rb
@@ -3,7 +3,7 @@
 require 'yaml'
 
 # Create tag directory if it doesn't exist
-Dir.mkdir('tags') unless Dir.exist?('tags')
+Dir.mkdir('_tags') unless Dir.exist?('_tags')
 
 # Get all tags from posts
 all_tags = []
@@ -19,14 +19,18 @@ Dir.glob('_posts/*').each do |post_file|
 end
 
 all_tags.uniq.each do |tag|
+  # 첫 글자(이모지)를 제외한 태그 텍스트 추출
+  tag_without_emoji = tag[1..-1]
+  # 태그 슬러그 생성
+  tag_slug = tag_without_emoji.strip.gsub(/\s+/, '-').gsub(/[^\w\-]/, '').downcase
+  
   # Create tag page if it doesn't exist
-  tag_filename = tag.gsub(/[^\w]/, '-').downcase
-  tag_file = "tags/#{tag_filename}.md"
+  tag_file = "_tags/#{tag_slug}.md"
   
   unless File.exist?(tag_file)
     File.open(tag_file, 'w') do |file|
-      file.write("---\nlayout: tag\ntag: #{tag}\npermalink: /tags/#{tag_filename}/\n---\n")
+      file.write("---\nlayout: tag\ntag: \"#{tag}\"\npermalink: /tags/#{tag_slug}/\n---\n")
     end
-    puts "Created tag page for: #{tag}"
+    puts "Created tag page for: #{tag} (slug: #{tag_slug})"
   end
 end
